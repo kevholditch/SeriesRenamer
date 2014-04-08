70abb41f91a05903afe74ab81c967b62df95c272
kevholditch <kevin.holditch@euromoneyplc.com>
2014-04-08 09:23:24 +0100

    Updated test so that its now failing

--- src/fileFetcher.js
+++ src/fileFetcher.js
@@ -1,9 +1,18 @@
 
-define(['fs'], function() {
+define(['fs', 'q'], function(fs, $q) {
 	'use strict';
 
 	var fetchFiles = function(dir) {
-		return [];
+		var deferred = $q.defer();
+
+		fs.readdir(dir, function(err, files){			
+			for(var i=0; i<files.length; i++){
+				console.log(files[i]);				
+			}
+			deferred.resolve({episodes: files});
+		});
+
+		return deferred.promise;		
 	};	
 
 	return {
